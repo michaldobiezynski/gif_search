d429f349f2afc25e2ae794350de515ccbecee030
michaldobiezynski <dobiezynski@gmail.com>
2019-11-07 20:56:45 +0000

    fetching data from gif api and loging it to console

--- src/App.js
+++ src/App.js
@@ -2,14 +2,32 @@ import React, { Component } from 'react';
 import './App.css';
 import SearchForm from './Components/SearchForm';
 import GifList from './Components/GifList';
+import axios from 'axios';
 
 export default class App extends Component {
   
   constructor() {
     super();
-  } 
+    this.state = {
+        gifs: []
+    };
+  }
 
-  render() { 
+  componentDidMount() {
+      fetch
+      ('http://api.giphy.com/v1/gifs/trending?api_key=JxdrbxzwdKM1QjPWfftipzRFIb51wAyg')
+          .then(response => response.json())
+          .then(responseData => {
+              this.setState({ gifs: responseData.data });
+          })
+          .catch(error => {
+              console.log('Error fetching and parsin data', error);
+          });
+  }
+
+
+    render() {
+      console.log(this.state.gifs);
     return (
       <div>
         <div className="main-header">
