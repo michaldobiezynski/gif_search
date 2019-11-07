b45836b4cd97878b34eefcd509fb073ab910f605
michaldobiezynski <dobiezynski@gmail.com>
2019-11-07 21:27:00 +0000

    created a new function that will be used for searching, made the url for fetch be a template literal to allow it to accept a variable as query parameter

--- src/App.js
+++ src/App.js
@@ -24,6 +24,17 @@ export default class App extends Component {
           });
   }
 
+  performSearch = (query) => {
+      fetch
+      (`http://api.giphy.com/v1/gifs/search?q=${query}&limit=24&api_key=JxdrbxzwdKM1QjPWfftipzRFIb51wAyg`)
+          .then(response => response.json())
+          .then(responseData => {
+              this.setState({ gifs: responseData.data });
+          })
+          .catch(error => {
+              console.log('Error fetching and parsin data', error);
+          });
+  }
 
     render() {
       console.log(this.state.gifs);
